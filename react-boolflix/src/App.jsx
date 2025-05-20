8757589db052afdb8bbd197ae57d3d553346f23a
tizianotambone <tambonetiziano@gmail.com>
2025-05-20 12:12:24 +0200

    first commit

---
+++ react-boolflix/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SearchBar from './component/SearchBar'
+
+function App() {
+  
+
+  return (
+    <>
+      <SearchBar/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ react-boolflix/src/component/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+
+function SearchBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchBar
